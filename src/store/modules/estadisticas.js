032882bbd2f701d5ad08190ce40e8828dd59b1ea
Cristian Ayala <cristian.ayala@ulinksolution.com>
2023-11-09 19:51:57 -0600

    feat(estadisticas): filtros de estadisticas se completan

--- src/store/modules/estadisticas.js
+++ src/store/modules/estadisticas.js
@@ -1,16 +1,19 @@
-// import { ElMessage } from "element-plus";
-// import router from "../../router/index";
+import { apolloClient } from "@/plugins/vue-apollo";
+import {
+  GET_STATISTICS,
+  GET_USERS,
+} from "@/store/graphql/queries/estadisticas";
+
 const isLoggedIn = () => false;
 
-const rejectPromise = (message) => new Promise((resolve, reject) => {
-  reject(new Error(message));
-});
+const rejectPromise = (message) =>
+  new Promise((resolve, reject) => {
+    reject(new Error(message));
+  });
 export default (app) => ({
   namespaced: true,
-  state: {
-  },
-  mutations: {
-  },
+  state: {},
+  mutations: {},
   actions: {
     getStatistics({ dispatch }, searchParameters) {
       if (!isLoggedIn()) return rejectPromise("No está logueado");
@@ -19,20 +22,97 @@ export default (app) => ({
         credentials: "include",
       };
       const role = localStorage.getItem("role");
-      if (searchParameters.userSelected === "Todos" && role === "manager") return dispatch("getStatisticsByOrgDiv", { searchParameters, settings });
-      if (searchParameters.typeOfFilter === "Por cede") return dispatch("getStatisticsByOrgDiv", { searchParameters, settings });
+      if (searchParameters.userSelected === "Todos" && role === "manager")
+        return dispatch("getStatisticsByOrgDiv", {
+          searchParameters,
+          settings,
+        });
+      if (searchParameters.typeOfFilter === "Por cede")
+        return dispatch("getStatisticsByOrgDiv", {
+          searchParameters,
+          settings,
+        });
       return dispatch("getStatisticsByUser", { searchParameters, settings });
     },
-    getStatisticsByUser(store, {searchParameters, settings}) {
-      return fetch(`${app.config.globalProperties.$url}ordenes/_design/estadisticasVenta/_view/estadisticasVentaByUser?startkey=[${searchParameters.userSelected !== "Todos" ? `"${searchParameters.userSelected}",` : "" }${searchParameters.startkey}]&endkey=[${searchParameters.userSelected !== "Todos" ? `"${searchParameters.userSelected}"` : JSON.stringify({}) },${searchParameters.endkey}]`, settings)
-        .then(response => response.json());
+    async GET_STATISTICS({ commit }, variables) {
+      try {
+        window.console.log(
+          "JSON.stringify(variables, null, 2)",
+          JSON.stringify(variables, null, 2),
+        );
+        const searchInfo = {
+          query: GET_STATISTICS,
+          fetchPolicy: "network-only",
+          variables,
+        };
+        const res = await apolloClient.query(searchInfo);
+        if (!res || res.errors)
+          throw new Error("al obtener estadísticas.\n", res.errors);
+        return {
+          numeroOrdenes: res.data.numeroOrdenes.aggregate.count,
+          maxTotal: res.data.maxTotal.aggregate.max.total_orden,
+          minTotal: res.data.minTotal.aggregate.min.total_orden,
+          totalOrdenes: res.data.totalOrdenes.aggregate.sum.total_orden,
+        };
+      } catch (err) {
+        window.console.error(err);
+        commit(
+          "common/errorNotification",
+          `Error al obtener detalle de la orden. ${err}`,
+          {
+            root: true,
+          },
+        );
+        return null;
+      }
     },
-    getStatisticsByOrgDiv(store, {searchParameters, settings}) {
+    async GET_USERS({ commit }, variables) {
+      try {
+        const searchInfo = {
+          query: GET_USERS,
+          variables,
+        };
+        const res = await apolloClient.query(searchInfo);
+        if (!res || res.errors)
+          throw new Error("al obtener usuarios.\n", res.errors);
+        return res.data.users;
+      } catch (err) {
+        window.console.error(err);
+        commit(
+          "common/errorNotification",
+          `Error al obtener usuarios. ${err}`,
+          {
+            root: true,
+          },
+        );
+        return null;
+      }
+    },
+    getStatisticsByUser(store, { searchParameters, settings }) {
+      return fetch(
+        `${
+          app.config.globalProperties.$url
+        }ordenes/_design/estadisticasVenta/_view/estadisticasVentaByUser?startkey=[${
+          searchParameters.userSelected !== "Todos"
+            ? `"${searchParameters.userSelected}",`
+            : ""
+        }${searchParameters.startkey}]&endkey=[${
+          searchParameters.userSelected !== "Todos"
+            ? `"${searchParameters.userSelected}"`
+            : JSON.stringify({})
+        },${searchParameters.endkey}]`,
+        settings,
+      ).then((response) => response.json());
+    },
+    getStatisticsByOrgDiv(store, { searchParameters, settings }) {
       let orgDivision = null;
-      if (searchParameters.orgDivSelected != null) orgDivision = searchParameters.orgDivSelected;
+      if (searchParameters.orgDivSelected != null)
+        orgDivision = searchParameters.orgDivSelected;
       else orgDivision = localStorage.getItem("org_division");
-      return fetch(`${app.config.globalProperties.$url}ordenes/_design/estadisticasVenta/_view/ordersByOrgDiv?startkey=["${orgDivision}",${searchParameters.startkey}]&endkey=["${orgDivision}",${searchParameters.endkey}]`, settings)
-        .then(response => response.json());
-    }
+      return fetch(
+        `${app.config.globalProperties.$url}ordenes/_design/estadisticasVenta/_view/ordersByOrgDiv?startkey=["${orgDivision}",${searchParameters.startkey}]&endkey=["${orgDivision}",${searchParameters.endkey}]`,
+        settings,
+      ).then((response) => response.json());
+    },
   },
 });
--- src/views/Estadisticas.vue
+++ src/views/Estadisticas.vue
@@ -4,14 +4,19 @@
       <div style="margin-top: 2rem">
         <div class="card">
           <div class="card-header">
-            <h6 class="text-uppercase mb-0" style="display: inline-block">Estadísticas</h6>
-
+            <h6 class="text-uppercase mb-0" style="display: inline-block">
+              Estadísticas
+            </h6>
           </div>
           <div class="card-body">
             <div class="filtros">
               <div class="d-inline-flex pr-2 pb-2">
-                <el-button type="primary" round @click="show.estadisticasFilterDrawer = true"
-                >Filtros ({{ allFilters.length }})</el-button>
+                <el-button
+                  type="primary"
+                  round
+                  @click="show.estadisticasFilterDrawer = true"
+                  >Filtros ({{ allFilters.length }})</el-button
+                >
               </div>
               <div
                 v-if="allFilters && allFilters.length > 0"
@@ -37,7 +42,7 @@
             <el-statistic :value="totalTillDate" :formatter="formatDecimal">
               <template #title>
                 <div style="display: inline-flex; align-items: center">
-                  {{totalTillDateLabel}}
+                  Venta Total
                 </div>
               </template>
             </el-statistic>
@@ -66,7 +71,7 @@
           </div>
         </el-col>
         <el-col :span="24">
-          <div class="statistic-card">
+          <div class="statistic-card" style="margin-bottom: 2rem">
             <el-statistic :value="minTotalOrder" :formatter="formatDecimal">
               <template #title>
                 <div style="display: inline-flex; align-items: center">
@@ -78,21 +83,17 @@
         </el-col>
       </div>
     </div>
-    <!-- eslint-disable-next-line vue/v-on-function-call -->
-    <estadisticas-filters ref="filterEstadistica" :show="show" @filters-confirmed="filtersConfirmed">
+    <estadisticas-filters
+      ref="filterEstadistica"
+      :show="show"
+      @filters-confirmed="filtersConfirmed"
+    >
     </estadisticas-filters>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
-
-function arrayEquals(a, b) {
-  return Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index]);
-}
 
 export default {
   name: "EstadisticasView",
@@ -106,73 +107,114 @@ export default {
     },
     selectedDate: null,
     filtersQuery: {},
+    loading: null,
   }),
   computed: {
-    totalTillDateLabel() {
-      return "Venta Total del día";
-    },
     allFilters() {
       const filters = [];
-      if (arrayEquals(this.filtersQuery.startkey, this.filtersQuery.endkey)) filters.push(`${this.filtersQuery.startkey[2]}/${this.filtersQuery.startkey[1]}/${this.filtersQuery.startkey[0]}`);
+      if (this.isSameDay(this.filtersQuery.fromDate, this.filtersQuery.toDate))
+        filters.push(this.getDayMonthYear(this.filtersQuery.fromDate));
       else {
-        filters.push(`Desde: ${this.filtersQuery.startkey[2]}/${this.filtersQuery.startkey[1]}/${this.filtersQuery.startkey[0]}`);
-        filters.push(`Hasta${this.filtersQuery.endkey[2]}/${this.filtersQuery.endkey[1]}/${this.filtersQuery.endkey[0]}`);
+        filters.push(
+          `Desde: ${this.getDayMonthYear(this.filtersQuery.fromDate)}`,
+        );
+        filters.push(`Hasta${this.getDayMonthYear(this.filtersQuery.toDate)}`);
       }
-      if (this.filtersQuery.typeOfFilter === "Por cede") filters.push(this.filtersQuery.orgDivSelected);
-      else filters.push(this.filtersQuery.userSelected);
+      filters.push(this.filtersQuery.orgDivSelected);
+      filters.push(this.filtersQuery.createdBy);
       return filters;
     },
   },
-  created() {
-    this.filtersConfirmed();
-  },
   methods: {
-    ...mapActions("estadisticas", ["getStatistics"]),
-    getDateFormatQuery(date) {
-      const day = date.getDate();
-      const month = date.getMonth() + 1;
-      const year = date.getFullYear();
-      return [year, month, day];
-    },
+    ...mapActions("estadisticas", ["GET_STATISTICS"]),
     getStartAndEndDate({ start, end }) {
-      // eslint-disable-next-line no-param-reassign
-      if (start == null) start = new Date();
-      // eslint-disable-next-line no-param-reassign
-      if (end == null) end = start;
+      /* eslint-disable no-param-reassign */
+      if (typeof start === "string") start = new Date(start);
+      else if (start == null) start = new Date();
+      if (typeof end === "string") end = new Date(end);
+      else if (end == null) end = start;
       return {
-        startkey: this.getDateFormatQuery(start),
-        endkey: this.getDateFormatQuery(end),
+        fromDate: new Date(
+          start.getFullYear(),
+          start.getMonth(),
+          start.getDate(),
+          0,
+          0,
+          0,
+          0,
+        ).toISOString(),
+        toDate: new Date(
+          end.getFullYear(),
+          end.getMonth(),
+          end.getDate(),
+          23,
+          59,
+          59,
+          999,
+        ).toISOString(),
       };
     },
-    getUserSelected(usr) {
-      if (usr != null) return usr;
-      return localStorage.getItem("user_name");
-    },
     setStatistics(res) {
-      const tmpRes = res.rows[0].value || {};
-      this.totalTillDate = tmpRes.sum;
-      this.countTillDate = tmpRes.count;
-      this.maxTotalOrder = tmpRes.max;
-      this.minTotalOrder = tmpRes.min;
+      this.totalTillDate = res.totalOrdenes || 0.00001;
+      this.countTillDate = res.numeroOrdenes || 0.00001;
+      this.maxTotalOrder = res.maxTotal || 0.00001;
+      this.minTotalOrder = res.minTotal || 0.00001;
     },
     setToZero() {
-      this.totalTillDate = 0;
-      this.countTillDate = 0;
-      this.maxTotalOrder = 0;
-      this.minTotalOrder = 0;
+      this.totalTillDate = 0.00001;
+      this.countTillDate = 0.00001;
+      this.maxTotalOrder = 0.00001;
+      this.minTotalOrder = 0.00001;
     },
     async filtersConfirmed(params) {
+      this.$nextTick(() => {
+        this.loading = this.$loading({ fullscreen: true });
+      });
+      // window.console.log(
+      //   "JSON.stringify(params, null, 2)",
+      //   JSON.stringify(params, null, 2),
+      // );
       const dates = this.getStartAndEndDate(params?.date || { start: null });
-      const filters = { ...dates, userSelected: this.getUserSelected(params?.userSelected) };
-      filters.typeOfFilter = params?.typeOfFilter || null;
-      if (filters.typeOfFilter === "Por cede") filters.orgDivSelected = params?.orgDivSelected;
-      this.filtersQuery = filters;
-      const res = await this.getStatistics(filters);
-      if (res?.rows?.[0]?.value != null) this.setStatistics(res);
+      const filters = {
+        ...dates,
+        createdBy: params?.userSelected,
+        orgDivSelected: params?.orgDivSelected,
+      };
+      if (Array.isArray(params?.orgDivSelected))
+        filters.orgDivSelected = "Todas las cedes";
+      this.filtersQuery = JSON.parse(JSON.stringify(filters));
+      let cedesSelected = filters.orgDivSelected;
+      if (cedesSelected === "Todas las cedes") {
+        delete filters.orgDivSelected;
+        cedesSelected = null;
+      } else if (typeof cedesSelected === "string") {
+        cedesSelected = [cedesSelected];
+      }
+      filters.cedes = cedesSelected;
+      const res = await this.GET_STATISTICS(filters);
+      if (res != null) this.setStatistics(res);
       else this.setToZero();
+      this.loading.close();
     },
     formatDecimal(value) {
+      if (!value) return null;
       return `$${value.toFixed(2)}`;
+    },
+    isSameDay(date1, date2) {
+      date1 = new Date(date1);
+      date2 = new Date(date2);
+      return (
+        date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate()
+      );
+    },
+    getDayMonthYear(date) {
+      date = new Date(date);
+      const day = date.getDate();
+      const month = date.getMonth() + 1; // Adding 1 because getMonth returns 0-indexed values (0-11).
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
     },
   },
 };
@@ -252,7 +294,7 @@ export default {
   text-align: center;
 }
 .card-header {
-  box-shadow: none
+  box-shadow: none;
 }
 .card-body {
   padding: 0%;
